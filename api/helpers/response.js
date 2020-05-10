9190971a2c8af87769fbaaf272ebe10c5444d18d
oloruntobi <awoderuoloruntobi@gmail.com>
2020-05-10 15:01:34 +0100

    Added response helpers

---
+++ api/helpers/response.js
@@ -0,0 +1,10 @@
+const successResponse = (res, status, data) => {
+    return res.status(status).json(data);
+  };
+  
+  const errorHelper = (res, status, error) => {
+    return res.status(status).json(error);
+  };
+  
+  module.exports = { successResponse, errorHelper };
+  
