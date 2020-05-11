d79d672af6e3da6e223433a145b463c8dd46ad9a
oloruntobi <awoderuoloruntobi@gmail.com>
2020-05-11 13:46:23 +0100

    Added update userprofile

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,14 +1,14 @@
-const models = require('../../models');
+const models = require("../../models");
 
-const { generateToken } = require('../helpers/jwt');
-const { successResponse, errorHelper } = require('../helpers/response');
+const { generateToken } = require("../helpers/jwt");
+const { successResponse, errorHelper } = require("../helpers/response");
 
 module.exports = {
   async createUser(req, res) {
     try {
       const user = await models.User.create(req.body);
       const token = await generateToken(user);
-      return successResponse(res, 201, { msg: 'Usercreated', token });
+      return successResponse(res, 201, { msg: "Usercreated", token });
     } catch (error) {
       return errorHelper(res, 500, error.message);
     }
@@ -16,24 +16,39 @@ module.exports = {
   async loginUser(req, res, next) {
     try {
       const user = await models.User.findOne({ email: req.body.email }).select(
-        '+password'
+        "+password"
       );
 
       if (!user) {
-        return errorHelper(res, 401, 'Invalid credentials');
+        return errorHelper(res, 401, "Invalid credentials");
       }
       const confirm = user.comparePassword(req.body.password);
       if (!confirm) {
-        return errorHelper(res, 401, 'Invalid credentials');
+        return errorHelper(res, 401, "Invalid credentials");
       }
       const token = await generateToken(user);
 
       return successResponse(res, 200, {
-        message: 'successfully logged in',
+        message: "successfully logged in",
         token
       });
     } catch (error) {
       return errorHelper(res, 500, error.message);
+    }
+  },
+
+  async updateProfile(req, res, next) {
+    try {
+      const { user } = req;
+      const { password } = req.body;
+      if (password) {
+        req.body.password = bcrypt.hashSync(password, 10);
+      }
+      const updatedUser =  merge(user, req.body);
+      user.save();
+      return successResponse(res, 200, updatedUser);
+    } catch (error) {
+      return next({ message: "Error updating user profile" });
     }
   }
 };
